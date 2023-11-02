95a7f376bbd7db944e0ddf1292042259c850d8f2
monstro1 <michael@monstro1.com>
2023-11-02 15:32:32 -0400

    Add check for number button to userInput

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const divide = (a, b) => a / b;
 
 const operate = (a, b, op) => op(a, b);
 
-const numberedButtons = [...document.querySelectorAll(".number-btn")];
-
 const buttons = [...document.querySelectorAll("button")];
 
 buttons.forEach(button => {
@@ -17,5 +15,10 @@ const bigTextField = document.querySelector("#big-text");
 const smallTextField = document.querySelector("#small-text");
 
 function userInput(e) {
-	console.log(e.target.textContent);
+	let button = e.target;
+	if (button.classList.contains("number-btn")) {
+		console.log("number button");
+	} else {
+		console.log("some other button");
+	}
 }
